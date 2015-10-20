c865b89614aedd180b2022739a7054096f44d786
Ash Blue <ash@blueashes.com>
2015-10-20 06:27:29 -0700

    Checklist items can now be deleted

--- app/components/checklist-item.js
+++ app/components/checklist-item.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  //tagName: 'li'
+  actions: {
+    delete () {
+      if (confirm("Are you sure you want to delete this?"))
+        this.get('checklist').destroyRecord();
+    }
+  }
 });
