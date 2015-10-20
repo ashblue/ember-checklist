1ddb1bfdf621f17488d8cc8f8bd1a4dd9e1957ca
Ash Blue <ash@blueashes.com>
2015-10-20 04:00:43 -0700

    Cleaned up new checklist form into a separate component

--- app/components/new-checklist.js
+++ app/components/new-checklist.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
+/**
+ * Must be placed relative to a checklist template in order to work
+ */
 export default Ember.Component.extend({
+  newChecklist: {}, // We will bind our form to this
+
+  actions: {
+    submitChecklist () {
+      this.get('targetObject.store').createRecord('checklist', this.get('newChecklist')).save();
+
+      var $checklist = this.$('#checklist-form');
+      $checklist.find('input[type=text]').val('');
+      $checklist.focus();
+    }
+  }
 });
--- app/controllers/checklists.js
+++ app/controllers/checklists.js
@@ -1,16 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  newChecklist: {}, // We will bind our form to this
-
   actions: {
     removeChecklist: function () {
       this.destroy();
-    },
-
-    submitChecklist () {
-      this.store.createRecord('checklist', this.newChecklist).save();
-      $('#checklist-form').find('input[type=text]').val('');
     }
   }
 });
