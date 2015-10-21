0d5a1c2259671c96d0e6126adc38ad371a10ad07
Ash Blue <ash@blueashes.com>
2015-10-20 18:45:26 -0700

    Checklists now display their current number of items

--- app/components/checklist-item.js
+++ app/components/checklist-item.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  entriesEmpty: function () {
+    return this.get('checklist.entries').length === 0;
+  }.property('checklist.entries.length'),
+
   actions: {
-    delete () {
+    delete: function () {
       if (confirm("Are you sure you want to delete this?"))
         this.get('checklist').destroyRecord();
     }
