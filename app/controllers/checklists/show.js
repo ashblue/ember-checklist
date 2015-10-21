33e87cb1138d7743da13be89b2810f3030f1fa07
Ash Blue <ash@blueashes.com>
2015-10-21 10:11:21 -0700

    Title changes are now autosaved while typing

---
+++ app/controllers/checklists/show.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  showForm: false,
+
+  saveModal: function () {
+    var model = this.get('model');
+    if (model.isDirty) model.save();
+  },
+
+  actions: {
+    toggleForm: function () {
+      var showForm = this.get('showForm');
+      this.set('showForm', !showForm);
+    },
+
+    forceSave: function () {
+      var debounced = this.get('debounced');
+      if (debounced) Ember.run.cancel(debounced);
+      this.get('saveModal').call(this);
+    },
+
+    save: function () {
+      var debounced = Ember.run.debounce(this, this.get('saveModal'), 2000); // 2 seconds
+      this.set('debounced', debounced);
+    }
+  }
+});
