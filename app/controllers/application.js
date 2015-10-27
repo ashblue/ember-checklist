34225b23443b8aac24f055d667da89376b1dd14c
Ash Blue <ash@blueashes.com>
2015-10-27 04:43:39 -0700

    Current user data is now accessible globally

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,8 +5,13 @@ export default Ember.Controller.extend({
     session: Ember.inject.service('session'),
     isAuthenticated: function () {
         if (!env.authentication) return true;
-        return this.session.isAuthenticated;
-    },
+        return this.get('session.isAuthenticated');
+    }.property('session.isAuthenticated'),
+
+    current_user: function() {
+        var data = this.get('session.data.authenticated');
+        return data;
+    }.property('session.data.authenticated'),
 
     actions: {
         logout () {
