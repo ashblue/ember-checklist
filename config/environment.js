cedbcdc89519257ad15631bb89b7af1c5f02e797
Ash Blue <ash@blueashes.com>
2015-10-31 22:40:08 -0600

    Proper redirections for simple auth

--- config/environment.js
+++ config/environment.js
@@ -23,6 +23,12 @@ module.exports = function (environment) {
         }
     };
 
+    ENV['ember-simple-auth'] = {
+        authenticationRoute: 'auth/login',
+        routeAfterAuthentication: 'checklists',
+        routeIfAlreadyAuthenticated: 'checklists'
+    };
+
     if (environment === 'development') {
         // ENV.APP.LOG_RESOLVER = true;
         // ENV.APP.LOG_ACTIVE_GENERATION = true;
