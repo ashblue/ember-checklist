d13e3050657830c5ae210deacc8f315aa8c6f267
Ash Blue <ash@blueashes.com>
2015-10-31 18:46:15 -0600

    Preserved non-server testing environment
    Models only serialize now in specific environments

--- app/serializers/checklist.js
+++ app/serializers/checklist.js
@@ -1,8 +1,15 @@
 import DS from 'ember-data';
+import env from '../config/environment';
+
+var attrConfig = {};
+
+// Enable special environmental configs for development since we're faking a server
+if (env.environment === 'development') {
+    attrConfig.entries = { serialize: true };
+} else {
+    attrConfig.user = { serialize: true, key: 'user_id' };
+}
 
 export default DS.RESTSerializer.extend({
-    attrs: {
-        entries: { serialize: true },
-        user: { serialize: true, key: 'user_id' }
-    }
+    attrs: attrConfig
 });
--- app/serializers/entry.js
+++ app/serializers/entry.js
@@ -1,8 +1,15 @@
 import DS from 'ember-data';
+import env from '../config/environment';
 
-export default DS.RESTSerializer.extend({
-    attrs: {
+
+var attrConfig = {};
+if (env.environment !== 'development') {
+    attrConfig = {
         checklist: { serialize: true, key: 'checklist_id' },
         user: { serialize: true, key: 'user_id' }
-    }
+    };
+}
+
+export default DS.RESTSerializer.extend({
+    attrs: attrConfig
 });
