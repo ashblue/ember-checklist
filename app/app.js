a0a8e3a67ad4dabc795c15fe2b37c621e8e14910
Ash Blue <ash@blueashes.com>
2015-11-01 00:35:29 -0600

    Rails router shim adjustment

--- app/app.js
+++ app/app.js
@@ -13,6 +13,13 @@ App = Ember.Application.extend({
   Resolver: Resolver
 });
 
+// Fix Rails router issues with history shim
+if (config.environment === 'production') {
+  App.Router.reopen({
+    location: 'history'
+  });
+}
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
